6b29042df6d3a3caffc252e605ce6a225ddd2df0
FHaddad9 <ferashaddadgit@gmail.com>
2022-01-03 16:59:14 +0000

    Connect 4 game idea added

---
+++ src/Connect4/Play.java
@@ -0,0 +1,14 @@
+package Connect4;
+
+import java.util.Scanner;
+
+public class Play {
+	public static void main(String[] args) {
+		System.out.println("");
+		for(int i = 0; i < 6; i++) {
+			System.out.println("|( )( )( )( )( )( )( )|");
+		}
+		
+		
+	}
+}
