daef0725005cac566a3668cb8173e0499d399579
Fhaddad9 <ferashaddadgit@gmail.com>
2022-01-20 23:32:26 +0000

    Player input details added

--- src/Connect4/Connect4/CheckMove.c
+++ src/Connect4/Connect4/CheckMove.c
@@ -8,3 +8,21 @@ void checkValidSquare(square board[BOARD_X][BOARD_Y], player players[PLAYERS_NUM
 //
 //    }
 }
+
+void checkWin(square board[BOARD_X][BOARD_Y, player players[PLAYERS_NUM], int x, int y, int playerNum){
+    /*  x = 0, y = i
+        x = i, y = 0
+        x = i--, y = 0
+        x = 0, y = i--
+        x = i + 1, y = i + 1
+    */
+    if(board[x][y] == players[0].playerColour){
+        // check to right
+        for(int i = y; i < y + 4; i++){
+            if(board[x][i].squareColour == player)
+
+        }
+
+    }
+
+}
